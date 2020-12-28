2623c3e0f28e7adf981aa9544adcec3b9b69adcd
BringtBlack <guerramatias>
2020-12-28 15:49:30 -0300

    Add favicon & title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Airmaster',
+      title: 'Air Master | Windows and Doors | Puerto Rico',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
