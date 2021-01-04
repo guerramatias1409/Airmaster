d70240d64b7706a958705d5e51b70926f8e8f56b
BringtBlack <guerramatias>
2021-01-04 16:33:32 -0300

    HomeFooterDesktopView.dart - adapt sizes

--- lib/Views/HomeFooter/HomeFooterDesktopView.dart
+++ lib/Views/HomeFooter/HomeFooterDesktopView.dart
@@ -9,7 +9,7 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
   Widget build(BuildContext context,HomeFooterController controller) {
     return Material(
       child: Container(
-        height: 300,
+        height: 400,
         color: Color(0xFF5C6061),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
@@ -22,11 +22,11 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
                 children: [
                   Text(
                     "San Juan Showroom",
-                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    style: TextStyle(fontSize: 25, color: Colors.white),
                   ),
                   Container(
-                    height: 70,
-                    width: 250,
+                    height: 80,
+                    width: 350,
                     decoration: BoxDecoration(
                         image: DecorationImage(
                             fit: BoxFit.cover,
@@ -43,66 +43,69 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Container(
-                          height: 180,
-                          width: 200,
+                          height: 280,
+                          width: 350,
                           decoration: BoxDecoration(
                               image: DecorationImage(
                                   fit: BoxFit.cover,
                                   image:
                                   AssetImage('assets/Footer/footer.jpg')))),
-                      SizedBox(width: 10),
+                      SizedBox(width: 30),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Text(
                             "Follow us on social media",
-                            style: TextStyle(fontSize: 13, color: Colors.white),
+                            style: TextStyle(fontSize: 22, color: Colors.white),
                           ),
                           SizedBox(height: 10),
                           Row(
                             children: [
                               Container(
-                                height: 30,
-                                width: 30,
+                                height: 50,
+                                width: 50,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.all(Radius.circular(3)),
                                   color: Colors.white,
                                 ),
                                 child: Center(
                                   child: Icon(MdiIcons.facebook,
-                                      color: Color(0xFF5C6061)),
+                                      color: Color(0xFF5C6061),
+                                  size: 45),
                                 ),
                               ),
-                              SizedBox(width: 10),
+                              SizedBox(width: 20),
                               Container(
-                                  height: 30,
-                                  width: 30,
+                                  height: 50,
+                                  width: 50,
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.all(Radius.circular(8)),
                                     color: Colors.white,
                                   ),
                                   child: Center(
                                     child: Icon(MdiIcons.instagram,
-                                        color: Color(0xFF5C6061)),
+                                        color: Color(0xFF5C6061),
+                                    size: 45),
                                   )),
-                              SizedBox(width: 10),
+                              SizedBox(width: 20),
                               Container(
-                                  height: 30,
-                                  width: 30,
+                                  height: 50,
+                                  width: 50,
                                   color: Colors.white,
                                   child: Center(
                                     child: Icon(MdiIcons.youtube,
-                                        color: Color(0xFF5C6061)),
+                                        color: Color(0xFF5C6061),
+                                    size: 45),
                                   ))
                             ],
                           ),
-                          SizedBox(height: 10),
+                          SizedBox(height: 20),
                           //TODO revisar icono y texto
                           Container(
                             padding: EdgeInsets.symmetric(vertical: 2),
-                            width: 100,
-                            height: 30,
+                            width: 120,
+                            height: 35,
                             child: FloatingActionButton.extended(
                               heroTag: 'location',
                               shape: RoundedRectangleBorder(
@@ -112,18 +115,18 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
                               icon: Icon(Icons.location_on, size: 12),
                               label: Text("San Juan",
                                   style: TextStyle(
-                                      fontSize: 14, color: Colors.white)),
+                                      fontSize: 18, color: Colors.white)),
                               onPressed: () {},
                             ),
                           ),
                           SizedBox(height: 10),
                           Text(
                             "Ave de Diego 258 Puerto Nuevo",
-                            style: TextStyle(fontSize: 13, color: Colors.white),
+                            style: TextStyle(fontSize: 22, color: Colors.white),
                           ),
                           Text(
                             "San Juan, PR 00921",
-                            style: TextStyle(fontSize: 13, color: Colors.white),
+                            style: TextStyle(fontSize: 22, color: Colors.white),
                           ),
                           SizedBox(height: 10),
                           Row(
@@ -131,7 +134,7 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
                               Text(
                                 "787 999 0717",
                                 style: TextStyle(
-                                    fontSize: 15, color: Colors.white),
+                                    fontSize: 22, color: Colors.white),
                               ),
                               SizedBox(width: 20),
                               //TODO ver por que no funciona el HomeLeadingDivider
@@ -140,7 +143,7 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
                               Text(
                                 "787 999 0719",
                                 style: TextStyle(
-                                    fontSize: 15, color: Colors.white),
+                                    fontSize: 22, color: Colors.white),
                               )
                             ],
                           )
@@ -150,7 +153,7 @@ class HomeFooterDesktopView extends View<HomeFooterController>{
                   ),
                   Text(
                     "Air Master © 2020 All rights reserved",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
+                    style: TextStyle(fontSize: 22, color: Colors.white),
                   )
                 ],
               )
