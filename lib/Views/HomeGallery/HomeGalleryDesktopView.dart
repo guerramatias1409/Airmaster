15eda61ff79facfaf986ab26086c9fc2a6926af8
BringtBlack <guerramatias>
2021-01-04 16:11:36 -0300

    HomeGalleryDesktopView.dart - adapt sizes

--- lib/Views/HomeGallery/HomeGalleryDesktopView.dart
+++ lib/Views/HomeGallery/HomeGalleryDesktopView.dart
@@ -25,35 +25,83 @@ class HomeGalleryDesktopView extends View<HomeGalleryController>{
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 100, right: 100, bottom: 50),
-              child: GridView.count(
-                shrinkWrap: true,
-                mainAxisSpacing: 40,
-                crossAxisSpacing: 40,
-                crossAxisCount: 3,
-                childAspectRatio: 1.8,
+              padding: const EdgeInsets.only(left: 140, right: 140, bottom: 120),
+              child: Column(
                 children: [
-                  GalleryItem(image: 'assets/Home/gallery1.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery2.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery3.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery4.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery5.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery6.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery7.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery8.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery9.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery10.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery11.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery12.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery13.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery14.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery15.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery16.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery17.jpg'),
-                  GalleryItem(image: 'assets/Home/gallery18.jpg'),
+                  Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceBetween,
+                      children: [
+                        GalleryItem(image: 'assets/Home/gallery1.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery2.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery3.jpg'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 55),
+                  Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceBetween,
+                      children: [
+                        GalleryItem(image: 'assets/Home/gallery4.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery5.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery6.jpg'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 55),
+                  Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceBetween,
+                      children: [
+                        GalleryItem(image: 'assets/Home/gallery7.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery8.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery9.jpg'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 55),
+                  Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceBetween,
+                      children: [
+                        GalleryItem(image: 'assets/Home/gallery10.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery11.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery12.jpg'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 55),
+                  Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceBetween,
+                      children: [
+                        GalleryItem(image: 'assets/Home/gallery13.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery14.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery15.jpg'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 55),
+                  Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceBetween,
+                      children: [
+                        GalleryItem(image: 'assets/Home/gallery16.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery17.jpg'),
+                        GalleryItem(image: 'assets/Home/gallery18.jpg'),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
--- lib/Widgets/GalleryItem.dart
+++ lib/Widgets/GalleryItem.dart
@@ -13,8 +13,8 @@ class _GalleryItemState extends State<GalleryItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 50,
-        width: 100,
+        height: 245,
+        width: 370,
         decoration: BoxDecoration(
             image: DecorationImage(
                 fit: BoxFit.cover, image: AssetImage(widget.image))));
