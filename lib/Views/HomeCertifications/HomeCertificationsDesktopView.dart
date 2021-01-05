1569d83664ccd069d7ade4fa0e199ecdf041a540
BringtBlack <guerramatias>
2021-01-05 15:06:07 -0300

    HomeCertificationsMobileView.dart

--- lib/Views/HomeCertifications/HomeCertificationsDesktopView.dart
+++ lib/Views/HomeCertifications/HomeCertificationsDesktopView.dart
@@ -7,86 +7,84 @@ import 'package:airmaster/responsive/view.dart';
 class HomeCertificationsDesktopView extends View<HomeCertificationsController>{
   @override
   Widget build(BuildContext context,HomeCertificationsController controller) {
-    return Material(
-      child: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
-              child: Container(
-                height: 113,
-                child: Center(
-                  child: Text(
-                    "CERTIFICATIONS",
-                    style: TextStyle(fontSize: 50),
-                  ),
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Container(
+              height: 113,
+              child: Center(
+                child: Text(
+                  "CERTIFICATIONS",
+                  style: TextStyle(fontSize: 50),
                 ),
               ),
             ),
-            Container(
-              height: 470,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: Row(
-                  children: [
-                    Flexible(
-                      flex: 2,
-                      child: Container(
-                        color: Colors.white,
-                        width: double.infinity,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 60),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 50),
-                              Text(
-                                "IMPACT TEST VIDEO DEMONSTRATION",
+          ),
+          Container(
+            height: 470,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.white,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 60),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 50),
+                            Text(
+                              "IMPACT TEST VIDEO DEMONSTRATION",
+                              style: TextStyle(
+                                  fontSize: 46, fontWeight: FontWeight.bold, height: 1),
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Container(
+                              constraints: BoxConstraints(maxWidth: 400),
+                              child: Text(
+                                "Testing process to meet the Miami Dade County regulations",
                                 style: TextStyle(
-                                    fontSize: 46, fontWeight: FontWeight.bold, height: 1),
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w400,
+                                height: 1),
                               ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Container(
-                                constraints: BoxConstraints(maxWidth: 400),
-                                child: Text(
-                                  "Testing process to meet the Miami Dade County regulations",
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w400,
-                                  height: 1),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 40,
-                              ),
-                              SquaredButton(
-                                text: "VIEW\nVIDEO",
-                              )
-                            ],
-                          ),
+                            ),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            SquaredButton(
+                              text: "VIEW\nVIDEO",
+                            )
+                          ],
                         ),
                       ),
                     ),
-                    Flexible(
-                      flex: 3,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage(
-                                    'assets/Home/certificationvideoimage.jpg'))),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                  'assets/Home/certificationvideoimage.jpg'))),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/Views/HomeCertifications/HomeCertificationsMobileView.dart
+++ lib/Views/HomeCertifications/HomeCertificationsMobileView.dart
@@ -1,3 +1,4 @@
+import 'package:airmaster/Widgets/SquaredButton.dart';
 import 'package:flutter/material.dart';
 import 'package:airmaster/Controllers/HomeCertificationsController.dart';
 import 'package:airmaster/responsive/view.dart';
@@ -6,8 +7,82 @@ class HomeCertificationsMobileView extends View<HomeCertificationsController>{
   @override
   Widget build(BuildContext context,HomeCertificationsController controller) {
     return Container(
-      height: 200,
-      color: Colors.yellow,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Container(
+              height: 30,
+              child: Center(
+                child: Text(
+                  "CERTIFICATIONS",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 180,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.white,
+                      width: double.infinity,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 15),
+                          Text(
+                            "IMPACT TEST VIDEO DEMONSTRATION",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold, height: 1),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Container(
+                            constraints: BoxConstraints(maxWidth: 150),
+                            child: Text(
+                              "Testing process to meet the Miami Dade County regulations",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  height: 1),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          SquaredButton(
+                            text: "VIEW\nVIDEO",
+                            isMobile: true
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                  'assets/Home/certificationvideoimage.jpg'))),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
