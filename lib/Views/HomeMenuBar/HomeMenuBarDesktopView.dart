6fd6642f8a9a0f8c86354dfa20cd30c270f28e01
BringtBlack <guerramatias>
2021-01-05 19:58:53 -0300

    HomeMenuBarMobileView.dart - drawer (WIP)

--- lib/Views/HomeMenuBar/HomeMenuBarDesktopView.dart
+++ lib/Views/HomeMenuBar/HomeMenuBarDesktopView.dart
@@ -6,111 +6,109 @@ import 'package:airmaster/responsive/view.dart';
 class HomeMenuBarDesktopView extends View<HomeMenuBarController>{
   @override
   Widget build(BuildContext context,HomeMenuBarController controller) {
-    return Material(
-      child: Container(
-        height: 115,
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 55.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 250,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.contain,
-                        image: AssetImage('assets/logo.png'))),
-              ),
-              Row(
-                children: [
-                  InkWell(
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      splashColor: Colors.transparent,
-                      onTap: () {
-                        //Navigator.pushNamed(context, '/windows');
-                      },
-                      child: Text("WINDOWS",
-                        style: TextStyle(
-                            color: Color(0xFF5C6061).withOpacity(0.6),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300
-                        ),)),
-                  SizedBox(width: 45),
-                  InkWell(
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      splashColor: Colors.transparent,
-                      onTap: () {
-                        //Navigator.pushNamed(context, '/doors');
-                      },
-                      child: Text("DOORS",
-                          style: TextStyle(
-                              color: Color(0xFF5C6061).withOpacity(0.6),
-                              fontSize: 18,
-                              fontWeight: FontWeight.w300
-                          ))),
-                  SizedBox(width: 45),
-                  InkWell(
+    return Container(
+      height: 115,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 55.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 250,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.contain,
+                      image: AssetImage('assets/logo.png'))),
+            ),
+            Row(
+              children: [
+                InkWell(
                     highlightColor: Colors.transparent,
                     hoverColor: Colors.transparent,
                     splashColor: Colors.transparent,
                     onTap: () {
+                      //Navigator.pushNamed(context, '/windows');
                     },
-                    child: Text("RAILINGS & SHOWERS",
-                        style: TextStyle(
-                            color: Color(0xFF5C6061).withOpacity(0.6),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300
-                        )),
-                  ),
-                  SizedBox(width: 45),
-                  InkWell(
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    onTap: () {
-                    },
-                    child: Text("ABOUT US",
-                        style: TextStyle(
-                            color: Color(0xFF5C6061).withOpacity(0.6),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300
-                        )),
-                  ),
-                  SizedBox(width: 45),
-                  InkWell(
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    onTap: () {
-                    },
-                    child: Text("OUR GLASS",
-                        style: TextStyle(
-                            color: Color(0xFF5C6061).withOpacity(0.6),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300
-                        )),
-                  ),
-                  SizedBox(width: 45),
-                  InkWell(
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    onTap: () {
-                    },
-                    child: Text("CONTACT US",
-                        style: TextStyle(
-                            color: Color(0xFF5C6061).withOpacity(0.6),
+                    child: Text("WINDOWS",
+                      style: TextStyle(
+                          color: Color(0xFF5C6061).withOpacity(0.6),
                           fontSize: 18,
                           fontWeight: FontWeight.w300
-                        )),
-                  ),
-                ],
-              )
-            ],
-          ),
+                      ),)),
+                SizedBox(width: 45),
+                InkWell(
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      //Navigator.pushNamed(context, '/doors');
+                    },
+                    child: Text("DOORS",
+                        style: TextStyle(
+                            color: Color(0xFF5C6061).withOpacity(0.6),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300
+                        ))),
+                SizedBox(width: 45),
+                InkWell(
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                  },
+                  child: Text("RAILINGS & SHOWERS",
+                      style: TextStyle(
+                          color: Color(0xFF5C6061).withOpacity(0.6),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300
+                      )),
+                ),
+                SizedBox(width: 45),
+                InkWell(
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                  },
+                  child: Text("ABOUT US",
+                      style: TextStyle(
+                          color: Color(0xFF5C6061).withOpacity(0.6),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300
+                      )),
+                ),
+                SizedBox(width: 45),
+                InkWell(
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                  },
+                  child: Text("OUR GLASS",
+                      style: TextStyle(
+                          color: Color(0xFF5C6061).withOpacity(0.6),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300
+                      )),
+                ),
+                SizedBox(width: 45),
+                InkWell(
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                  },
+                  child: Text("CONTACT US",
+                      style: TextStyle(
+                          color: Color(0xFF5C6061).withOpacity(0.6),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300
+                      )),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/Views/HomeMenuBar/HomeMenuBarMobileView.dart
+++ lib/Views/HomeMenuBar/HomeMenuBarMobileView.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:airmaster/Controllers/HomeMenuBarController.dart';
 import 'package:airmaster/responsive/view.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeMenuBarMobileView extends View<HomeMenuBarController>{
   @override
   Widget build(BuildContext context,HomeMenuBarController controller) {
     return Container(
-      height: 40,
-      color: Colors.grey,
+      height: 50,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+                icon: Icon(MdiIcons.menu, color: Color(0xFF5C6061),size: 25),
+                onPressed: null),
+            Container(
+              width: 180,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.contain,
+                      image: AssetImage('assets/logo.png'))),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
