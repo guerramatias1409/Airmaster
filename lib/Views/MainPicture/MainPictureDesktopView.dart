328c57a49757a512ec390c0b6911f7d3daafe95f
BringtBlack <guerramatias>
2021-01-05 13:09:20 -0300

    MainPictureMobileView.dart

--- lib/Views/MainPicture/MainPictureDesktopView.dart
+++ lib/Views/MainPicture/MainPictureDesktopView.dart
@@ -6,62 +6,60 @@ import 'package:airmaster/responsive/view.dart';
 class MainPictureDesktopView extends View<MainPictureController>{
   @override
   Widget build(BuildContext context,MainPictureController controller) {
-    return Material(
-      child: Stack(
-        alignment: AlignmentDirectional.centerStart,
-        children: [
-          Container(
-              height: 830,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.cover, image: AssetImage('assets/Home/mainpicture.jpg')))
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 55),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "HIGH PERFORMANCE",
-                      style: TextStyle(
-                          fontSize: 54,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300
-                      ),
+    return Stack(
+      alignment: AlignmentDirectional.centerStart,
+      children: [
+        Container(
+            height: 830,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage('assets/Home/mainpicture.jpg')))
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 55),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "HIGH PERFORMANCE",
+                    style: TextStyle(
+                        fontSize: 54,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300
                     ),
-                    Text(
-                      "WINDOWS & DOORS",
-                      style: TextStyle(
-                          fontSize: 46,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "IMPACT AND HURRICANE\nRESISTANCE CERTIFICATION",
-                  style: TextStyle(
-                      fontSize: 28,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w100,
-                    height: 1
                   ),
+                  Text(
+                    "WINDOWS & DOORS",
+                    style: TextStyle(
+                        fontSize: 46,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "IMPACT AND HURRICANE\nRESISTANCE CERTIFICATION",
+                style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w100,
+                  height: 1
                 ),
-                SizedBox(
-                  height: 45,
-                ),
-                SquaredButton(text: "LEARN\nMORE")
-              ],
-            ),
-          )
-        ],
-      ),
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              SquaredButton(text: "LEARN\nMORE")
+            ],
+          ),
+        )
+      ],
     );
   }
 }
--- lib/Views/MainPicture/MainPictureMobileView.dart
+++ lib/Views/MainPicture/MainPictureMobileView.dart
@@ -1,3 +1,4 @@
+import 'package:airmaster/Widgets/SquaredButton.dart';
 import 'package:flutter/material.dart';
 import 'package:airmaster/Controllers/MainPictureController.dart';
 import 'package:airmaster/responsive/view.dart';
@@ -5,9 +6,60 @@ import 'package:airmaster/responsive/view.dart';
 class MainPictureMobileView extends View<MainPictureController>{
   @override
   Widget build(BuildContext context,MainPictureController controller) {
-    return Container(
-      height: 40,
-      color: Colors.cyan,
+    return Stack(
+      alignment: AlignmentDirectional.centerStart,
+      children: [
+        Container(
+            height: 250,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage('assets/Home/mainpicture.jpg')))
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "HIGH PERFORMANCE",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300
+                    ),
+                  ),
+                  Text(
+                    "WINDOWS & DOORS",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "IMPACT AND HURRICANE\nRESISTANCE CERTIFICATION",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w100,
+                    height: 1
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SquaredButton(text: "LEARN\nMORE", isMobile: true)
+            ],
+          ),
+        )
+      ],
     );
   }
 }
