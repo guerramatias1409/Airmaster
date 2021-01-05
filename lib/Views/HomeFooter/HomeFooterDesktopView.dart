818403f4f65c97870f7e097378c63b33589b8eca
BringtBlack <guerramatias>
2021-01-05 17:40:44 -0300

    HomeFooterMobileView.dart

--- lib/Views/HomeFooter/HomeFooterDesktopView.dart
+++ lib/Views/HomeFooter/HomeFooterDesktopView.dart
@@ -7,158 +7,156 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 class HomeFooterDesktopView extends View<HomeFooterController>{
   @override
   Widget build(BuildContext context,HomeFooterController controller) {
-    return Material(
-      child: Container(
-        height: 400,
-        color: Color(0xFF5C6061),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    "San Juan Showroom",
-                    style: TextStyle(fontSize: 25, color: Colors.white),
-                  ),
-                  Container(
-                    height: 80,
-                    width: 350,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('assets/whitelogo.png'))),
-                  )                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                          height: 280,
-                          width: 350,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image:
-                                  AssetImage('assets/Footer/footer.jpg')))),
-                      SizedBox(width: 30),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Follow us on social media",
-                            style: TextStyle(fontSize: 22, color: Colors.white),
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Container(
+    return Container(
+      height: 400,
+      color: Color(0xFF5C6061),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "San Juan Showroom",
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+                Container(
+                  height: 80,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/whitelogo.png'))),
+                )                ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                        height: 280,
+                        width: 350,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image:
+                                AssetImage('assets/Footer/footer.jpg')))),
+                    SizedBox(width: 30),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Follow us on social media",
+                          style: TextStyle(fontSize: 22, color: Colors.white),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(3)),
+                                color: Colors.white,
+                              ),
+                              child: Center(
+                                child: Icon(MdiIcons.facebook,
+                                    color: Color(0xFF5C6061),
+                                size: 45),
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            Container(
                                 height: 50,
                                 width: 50,
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(3)),
+                                  borderRadius: BorderRadius.all(Radius.circular(8)),
                                   color: Colors.white,
                                 ),
                                 child: Center(
-                                  child: Icon(MdiIcons.facebook,
+                                  child: Icon(MdiIcons.instagram,
                                       color: Color(0xFF5C6061),
                                   size: 45),
-                                ),
-                              ),
-                              SizedBox(width: 20),
-                              Container(
-                                  height: 50,
-                                  width: 50,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                                    color: Colors.white,
-                                  ),
-                                  child: Center(
-                                    child: Icon(MdiIcons.instagram,
-                                        color: Color(0xFF5C6061),
-                                    size: 45),
-                                  )),
-                              SizedBox(width: 20),
-                              Container(
-                                  height: 50,
-                                  width: 50,
-                                  color: Colors.white,
-                                  child: Center(
-                                    child: Icon(MdiIcons.youtube,
-                                        color: Color(0xFF5C6061),
-                                    size: 45),
-                                  ))
-                            ],
+                                )),
+                            SizedBox(width: 20),
+                            Container(
+                                height: 50,
+                                width: 50,
+                                color: Colors.white,
+                                child: Center(
+                                  child: Icon(MdiIcons.youtube,
+                                      color: Color(0xFF5C6061),
+                                  size: 45),
+                                ))
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        //TODO revisar icono y texto
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 2),
+                          width: 120,
+                          height: 35,
+                          child: FloatingActionButton.extended(
+                            heroTag: 'location',
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(0))),
+                            backgroundColor: Color(0xFF48A742),
+                            icon: Icon(Icons.location_on, size: 12),
+                            label: Text("San Juan",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white)),
+                            onPressed: () {},
                           ),
-                          SizedBox(height: 20),
-                          //TODO revisar icono y texto
-                          Container(
-                            padding: EdgeInsets.symmetric(vertical: 2),
-                            width: 120,
-                            height: 35,
-                            child: FloatingActionButton.extended(
-                              heroTag: 'location',
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(0))),
-                              backgroundColor: Color(0xFF48A742),
-                              icon: Icon(Icons.location_on, size: 12),
-                              label: Text("San Juan",
-                                  style: TextStyle(
-                                      fontSize: 18, color: Colors.white)),
-                              onPressed: () {},
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Ave de Diego 258 Puerto Nuevo",
+                          style: TextStyle(fontSize: 22, color: Colors.white, height: 1),
+                        ),
+                        Text(
+                          "San Juan, PR 00921",
+                          style: TextStyle(fontSize: 22, color: Colors.white),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              "787 999 0717",
+                              style: TextStyle(
+                                  fontSize: 22, color: Colors.white),
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            "Ave de Diego 258 Puerto Nuevo",
-                            style: TextStyle(fontSize: 22, color: Colors.white, height: 1),
-                          ),
-                          Text(
-                            "San Juan, PR 00921",
-                            style: TextStyle(fontSize: 22, color: Colors.white),
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Text(
-                                "787 999 0717",
-                                style: TextStyle(
-                                    fontSize: 22, color: Colors.white),
-                              ),
-                              SizedBox(width: 20),
-                              //TODO ver por que no funciona el HomeLeadingDivider
-                              Text("|", style: TextStyle(color: Colors.white)),
-                              SizedBox(width: 20),
-                              Text(
-                                "787 999 0719",
-                                style: TextStyle(
-                                    fontSize: 22, color: Colors.white),
-                              )
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  Text(
-                    "Air Master © 2020 All rights reserved",
-                    style: TextStyle(fontSize: 22, color: Colors.white),
-                  )
-                ],
-              )
-            ],
-          ),
+                            SizedBox(width: 20),
+                            //TODO ver por que no funciona el HomeLeadingDivider
+                            Text("|", style: TextStyle(color: Colors.white)),
+                            SizedBox(width: 20),
+                            Text(
+                              "787 999 0719",
+                              style: TextStyle(
+                                  fontSize: 22, color: Colors.white),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Text(
+                  "Air Master © 2020 All rights reserved",
+                  style: TextStyle(fontSize: 22, color: Colors.white),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/Views/HomeFooter/HomeFooterMobileView.dart
+++ lib/Views/HomeFooter/HomeFooterMobileView.dart
@@ -1,13 +1,164 @@
 import 'package:flutter/material.dart';
 import 'package:airmaster/Controllers/HomeFooterController.dart';
 import 'package:airmaster/responsive/view.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeFooterMobileView extends View<HomeFooterController>{
   @override
   Widget build(BuildContext context,HomeFooterController controller) {
     return Container(
-      height: 200,
-      color: Colors.brown,
+      height: 300,
+      color: Color(0xFF5C6061),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(),
+                Container(
+                  height: 40,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/whitelogo.png'))),
+                )
+              ],
+            ),
+            Text(
+              "San Juan Showroom",
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                        height: 160,
+                        width: 180,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image:
+                                AssetImage('assets/Footer/footer.jpg')))),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Follow us on social media",
+                          style: TextStyle(fontSize: 12, color: Colors.white),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(3)),
+                                color: Colors.white,
+                              ),
+                              child: Center(
+                                child: Icon(MdiIcons.facebook,
+                                    color: Color(0xFF5C6061),
+                                    size: 22),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                                  color: Colors.white,
+                                ),
+                                child: Center(
+                                  child: Icon(MdiIcons.instagram,
+                                      color: Color(0xFF5C6061),
+                                      size: 22),
+                                )),
+                            SizedBox(width: 10),
+                            Container(
+                                height: 30,
+                                width: 30,
+                                color: Colors.white,
+                                child: Center(
+                                  child: Icon(MdiIcons.youtube,
+                                      color: Color(0xFF5C6061),
+                                      size: 22),
+                                ))
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        //TODO revisar icono y texto
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 2),
+                          width: 75,
+                          height: 25,
+                          child: FloatingActionButton.extended(
+                            heroTag: 'location',
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(0))),
+                            backgroundColor: Color(0xFF48A742),
+                            icon: Icon(Icons.location_on, size: 12),
+                            label: Text("San Juan",
+                                style: TextStyle(
+                                    fontSize: 10, color: Colors.white)),
+                            onPressed: () {},
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Ave de Diego 258 Puerto Nuevo",
+                          style: TextStyle(fontSize: 12, color: Colors.white, height: 1),
+                        ),
+                        Text(
+                          "San Juan, PR 00921",
+                          style: TextStyle(fontSize: 12, color: Colors.white),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Text(
+                              "787 999 0717",
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.white),
+                            ),
+                            SizedBox(width: 10),
+                            //TODO ver por que no funciona el HomeLeadingDivider
+                            Text("|", style: TextStyle(color: Colors.white)),
+                            SizedBox(width: 10),
+                            Text(
+                              "787 999 0719",
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.white),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Air Master © 2020 All rights reserved",
+                          style: TextStyle(fontSize: 12, color: Colors.white),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
