f62f9d7c8648aa96ab4908c143ffd828629e85ca
BringtBlack <guerramatias>
2021-01-05 13:20:22 -0300

    HomeCategoriesDesktopView.dart - quit material

--- lib/Views/HomeCategories/HomeCategoriesDesktopView.dart
+++ lib/Views/HomeCategories/HomeCategoriesDesktopView.dart
@@ -6,38 +6,35 @@ import 'package:airmaster/responsive/view.dart';
 class HomeCategoriesDesktopView extends View<HomeCategoriesController>{
   @override
   Widget build(BuildContext context,HomeCategoriesController controller) {
-    return Material(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 22),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CategoryItem(
-              title: "WINDOWS",
-              route: '/windows',
-              image: 'assets/Home/categorywindows.jpg',
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 22),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CategoryItem(
+            title: "WINDOWS",
+            route: '/windows',
+            image: 'assets/Home/categorywindows.jpg',
+            comment: "HIGH PERFORMANCE WINDOWS",
+            subcomment: "Impact and Hurricane Resistance Certification",
+          ),
+          CategoryItem(
+              title: "DOORS",
+              route: '/doors',
+              image: 'assets/Home/categorydoors.jpg',
               comment: "HIGH PERFORMANCE WINDOWS",
-              subcomment: "Impact and Hurricane Resistance Certification",
-            ),
-            CategoryItem(
-                title: "DOORS",
-                route: '/doors',
-                image: 'assets/Home/categorydoors.jpg',
-                comment: "HIGH PERFORMANCE WINDOWS",
-                subcomment: "Impact and Hurricane Resistance Certification"),
-            CategoryItem(
-                title: "RAILINGS",
-                image: 'assets/Home/categoryrailings.jpg',
-                comment: "HIGH PERFORMANCE WINDOWS",
-                subcomment: "Impact and Hurricane Resistance Certification"),
-            CategoryItem(
-                title: "SHOWERS",
-                image: 'assets/Home/categoryshowers.jpg',
-                comment: "HIGH PERFORMANCE WINDOWS",
-                subcomment: "Impact and Hurricane Resistance Certification")
-          ],
-        ),
+              subcomment: "Impact and Hurricane Resistance Certification"),
+          CategoryItem(
+              title: "RAILINGS",
+              image: 'assets/Home/categoryrailings.jpg',
+              comment: "HIGH PERFORMANCE WINDOWS",
+              subcomment: "Impact and Hurricane Resistance Certification"),
+          CategoryItem(
+              title: "SHOWERS",
+              image: 'assets/Home/categoryshowers.jpg',
+              comment: "HIGH PERFORMANCE WINDOWS",
+              subcomment: "Impact and Hurricane Resistance Certification")
+        ],
       ),
     );
   }
