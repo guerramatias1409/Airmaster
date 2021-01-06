7a5f5b8d53bfb1fd3d9103117c39a8fa106b70c3
BringtBlack <guerramatias>
2021-01-06 12:48:30 -0300

    HomeMobileView.dart - add elevation to menu

--- lib/Views/Home/HomeMobileView.dart
+++ lib/Views/Home/HomeMobileView.dart
@@ -30,143 +30,152 @@ class HomeMobileView extends View<HomeController> {
                 HomeFooterBase()
               ],
             )),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 250),
-              color: Colors.white,
-              width: controller.width,
-              child: controller.openedMenu == false
-                  ? Container()
-                  : Container(
-                    height: MediaQuery.of(context).size.height,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      child: Column(
-                        children: [
-                          InkWell(
-                            onTap: () {},
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8.0),
-                              child: Text(
-                                "WINDOWS",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Color(0xFF5C6061).withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w300),
+            Material(
+              elevation: 10,
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 250),
+                color: Colors.white,
+                width: controller.width,
+                child: controller.openedMenu == false
+                    ? Container()
+                    : Container(
+                        height: MediaQuery.of(context).size.height,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          child: Column(
+                            children: [
+                              InkWell(
+                                onTap: () {},
+                                highlightColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 8.0),
+                                  child: Text(
+                                    "WINDOWS",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color:
+                                            Color(0xFF5C6061).withOpacity(0.6),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Divider(height: 2),
-                          InkWell(
-                            onTap: () {},
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8.0),
-                              child: Text(
-                                "DOORS",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Color(0xFF5C6061).withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w300),
+                              Divider(height: 2),
+                              InkWell(
+                                onTap: () {},
+                                highlightColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 8.0),
+                                  child: Text(
+                                    "DOORS",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color:
+                                            Color(0xFF5C6061).withOpacity(0.6),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Divider(height: 2),
-                          InkWell(
-                            onTap: () {},
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8.0),
-                              child: Text(
-                                "RAILINGS & SHOWERS",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Color(0xFF5C6061).withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w300),
+                              Divider(height: 2),
+                              InkWell(
+                                onTap: () {},
+                                highlightColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 8.0),
+                                  child: Text(
+                                    "RAILINGS & SHOWERS",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color:
+                                            Color(0xFF5C6061).withOpacity(0.6),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Divider(height: 2),
-                          InkWell(
-                            onTap: () {},
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8.0),
-                              child: Text(
-                                "ABOUT US",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Color(0xFF5C6061).withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w300),
+                              Divider(height: 2),
+                              InkWell(
+                                onTap: () {},
+                                highlightColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 8.0),
+                                  child: Text(
+                                    "ABOUT US",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color:
+                                            Color(0xFF5C6061).withOpacity(0.6),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Divider(height: 2),
-                          InkWell(
-                            onTap: () {},
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8.0),
-                              child: Text(
-                                "OUR GLASS",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Color(0xFF5C6061).withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w300),
+                              Divider(height: 2),
+                              InkWell(
+                                onTap: () {},
+                                highlightColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 8.0),
+                                  child: Text(
+                                    "OUR GLASS",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color:
+                                            Color(0xFF5C6061).withOpacity(0.6),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Divider(height: 2),
-                          InkWell(
-                            onTap: () {},
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8.0),
-                              child: Text(
-                                "CONTACT US",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: Color(0xFF5C6061).withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w300),
+                              Divider(height: 2),
+                              InkWell(
+                                onTap: () {},
+                                highlightColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 8.0),
+                                  child: Text(
+                                    "CONTACT US",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color:
+                                            Color(0xFF5C6061).withOpacity(0.6),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
                               ),
-                            ),
+                              Spacer(),
+                              IconButton(
+                                  icon: Icon(MdiIcons.arrowCollapseLeft),
+                                  color: Color(0xFF5C6061),
+                                  iconSize: 30,
+                                  onPressed: () {
+                                    controller.changeMenuBool();
+                                  })
+                            ],
                           ),
-                          Spacer(),
-                          IconButton(
-                              icon: Icon(MdiIcons.arrowCollapseLeft),
-                              color: Color(0xFF5C6061),
-                              iconSize: 30,
-                              onPressed: () {
-                                controller.changeMenuBool();
-                              })
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
+              ),
             )
           ],
         ),
